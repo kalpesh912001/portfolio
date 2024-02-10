6e7549e4f06f90cff63137b6eafdb4ab2713f49f
kalpesh patil <kalpeshspatil2001@gmail.com>
2024-02-10 12:03:14 +0530

    added fade in top & down animations

--- utils/motion.ts
+++ utils/motion.ts
@@ -37,3 +37,31 @@ export const slideInFromTop = {
         },
     },
 };
+
+export const fadeInTop = {
+    hidden: { y: -100, opacity: 0 },
+    visible: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            delay: 0.5,
+            duration: 0.5,
+            type: "spring", // You can adjust the transition type as needed
+            damping: 10, // You can adjust the damping for spring animation
+        },
+    },
+};
+
+export const fadeInDown = {
+    hidden: { y: -100, opacity: 0 },
+    visible: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            delay: 0.5,
+            duration: 0.5,
+            type: "spring", // You can adjust the transition type as needed
+            damping: 10, // You can adjust the damping for spring animation
+        },
+    },
+};
