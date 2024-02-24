b82f06f38e072b4af6304ebcb377005d201f8278
kalpesh <kalpeshspatil2001@gmail.com>
2024-02-25 00:03:56 +0530

    fixed footer

--- components/main/Footer/index.tsx
+++ components/main/Footer/index.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import {
-    RxDiscordLogo,
-    RxGithubLogo,
-    RxInstagramLogo,
-    RxTwitterLogo,
-    RxLinkedinLogo,
-} from "react-icons/rx";
-import { FaYoutube } from "react-icons/fa";
-
-
+import Image from 'next/image';
+import { Socials } from '@/constants';
+import phoneLogo from '../../../public/phone.svg'
+import mail from '../../../public/mail.svg';
 
 export default function index() {
     return (
         <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
             <div className="w-full flex flex-col items-center justify-center m-auto">
-                <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
-
-
-                    <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+                <div className="w-full h-full flex flex-row items-start justify-around flex-wrap">
+                    {/* <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
                         <div className="font-bold text-[16px]">Community</div>
                         <p className="flex flex-row items-center my-[15px] cursor-pointer">
                             <FaYoutube />
@@ -31,41 +23,37 @@ export default function index() {
                             <RxDiscordLogo />
                             <span className="text-[15px] ml-[6px]">Discord</span>
                         </p>
-                    </div>
+                    </div> */}
                     <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
                         <div className="font-bold text-[16px]">Social Media</div>
-                        <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                            <FaYoutube />
-                            <span className="text-[15px] ml-[6px]">Instagram</span>
-                        </p>
-                        <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                            <RxGithubLogo />
-                            <span className="text-[15px] ml-[6px]">Twitter</span>
-                        </p>
-                        <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                            <RxDiscordLogo />
-                            <span className="text-[15px] ml-[6px]">Linkedin</span>
-                        </p>
+                        {
+                            Socials.map((item, index) => {
+                                return (
+                                    <a href={item.link}
+                                        target="_blank"
+                                        className="flex flex-row items-center my-[15px] cursor-pointer">
+                                        <Image src={item.src} alt='' width={16} height={16} />
+                                        <span className="text-[15px] ml-[6px]">{item.name}</span>
+                                    </a>
+                                )
+                            })
+                        }
                     </div>
                     <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-                        <div className="font-bold text-[16px]">About</div>
+                        <div className="font-bold text-[16px]">Contact</div>
                         <p className="flex flex-row items-center my-[15px] cursor-pointer">
-
-                            <span className="text-[15px] ml-[6px]">Become Sponsor</span>
+                            <Image src={phoneLogo} alt='' width={16} height={16} />
+                            <span className="text-[15px] ml-[6px]">+91 8857896324</span>
                         </p>
                         <p className="flex flex-row items-center my-[15px] cursor-pointer">
-
-                            <span className="text-[15px] ml-[6px]">Learning about me</span>
-                        </p>
-                        <p className="flex flex-row items-center my-[15px] cursor-pointer">
-
-                            <span className="text-[15px] ml-[6px]">mifwebchain@gmail.com</span>
+                            <Image src={mail} alt='' width={16} height={16} />
+                            <span className="text-[15px] ml-[6px]">kalpeshspatil2001@gmail.com</span>
                         </p>
                     </div>
                 </div>
 
                 <div className="mb-[20px] text-[15px] text-center">
-                    &copy; WebChain Dev 2023 Inc. All rights reserved
+                    &copy; portfolio-lovat-zeta-28 Dev 2024 Inc. All rights reserved
                 </div>
             </div>
         </div>
